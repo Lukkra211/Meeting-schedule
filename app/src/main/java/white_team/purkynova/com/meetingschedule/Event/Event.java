be45543004f19c6c5bffa5dca989b231e4fd66de
Pavel <mr.p.balusek@gmail.com>
2017-10-30 21:18:00 +0100

    Created and upgradet Event class

--- app/src/main/java/white_team/purkynova/com/meetingschedule/Event/Event.java
+++ app/src/main/java/white_team/purkynova/com/meetingschedule/Event/Event.java
@@ -1,19 +1,31 @@
+package white_team.purkynova.com.meetingschedule.Event;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.lang.String;
 import java.util.Date;
 
-//HOTOVO AZ NEBUDOU KOMENTARE
 public class Event {
-//Vyzkouset
-	private Date since;
-	private Date till;
-	private String description;
-	private String lecturer;
-	private int eventType;
-    //HOTOVO
-    private void setType(String eventString){
+    // Save values to variables
+    private Date since;
+    private Date till;
+    private String place;
+    private String description;
+    private String lecturer;
+    private int eventType;
+
+    //Constructor for class (creates object)
+    Event(String since, String till, String eventType, String description,String place , String lecturer){
+            this.description = description;
+            this.lecturer = lecturer;
+            this.place = place;
+            setType(eventType);
+            setTime(till, since);
+    }
+
+
+    //Set type of event as intiger
+    private void setType(String eventString) {
         String[] types = {"lecture", "food", "free_time"};
         for (int i=0; i< 2; i++) {
             if (eventString == types[i]) {
@@ -21,58 +33,50 @@ public class Event {
             }
         }
     }
-    String getInfoText(){
+    //Change type of tyme from yyy-mm-dd T hh:mm Z to yyyy-MM-dd HH:mm:ss
+    private void setTime(String start, String end) {
+        try {
+            this.till = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(end);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
-        return this.description;
     }
 
-    Boolean isLecture(){
+    //Return true if event type is lecture
+    Boolean isLecture() {
         if(this.lecturer != null & this.eventType == 0) {
             return true;
         } else {
             return  false;
         }
     }
+    String getTimeSpan() {
+        DateFormat df = new SimpleDateFormat("hh:mm");
+        return df.format((this.since)) + " - " + df.format((this.till));
+    }
 
-    String getType(){
+    //Return Event type as String
+    String getType() {
         String[] types = {"lecture", "food", "free_time"};
         return types[this.eventType];
     }
-
-    String getLecturer(){
+    //If event type is lecturer, then return it´s name
+    String getLecturer() {
         if (isLecture()) {
             return this.lecturer;
         }
-        else{
+        else {
             return null;
         }
     }
-
-	//DOKONCIT
-    //zjistit jak se ma vratit cas
-	String getTimeSpan() {
-        DateFormat df = new SimpleDateFormat("hh:mm");
-		return df.format((this.since)) + " - " + df.format((this.till));
+    //Return place of event
+    String getPlace(){
+        return this.place;
     }
 
-	private void setTime(String start, String end){
-        //2017-10-17 T16:28Z
-	     try {
-	            //inputDate = "2010-01-04 01:32:27 UTC";
-	            this.till = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z").parse(end);
-	        } catch (ParseException e) {
-	            e.printStackTrace();
-	        }
-
-	}
-	
-	Event(String since, String till, String eventType, String description, String lecturer){
-		this.description = description;
-        this.lecturer = lecturer;
-        setType(eventType);
-        setTime(till, since);
-	}
-	
-	
+    //Return description of Event
+    String getInfoText(){
+        return this.description;
+    }
 }
-
