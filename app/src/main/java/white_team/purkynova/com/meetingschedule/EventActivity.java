4dabfc79d4e602e40c4417a17fff8c137aeebbbc
Hony <hony.com@seznam.cz>
2017-10-31 19:41:18 +0100

    + start codeing

--- app/src/main/java/white_team/purkynova/com/meetingschedule/EventActivity.java
+++ app/src/main/java/white_team/purkynova/com/meetingschedule/EventActivity.java
@@ -1,11 +1,73 @@
 package white_team.purkynova.com.meetingschedule;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import white_team.purkynova.com.meetingschedule.Event.Event;
+import white_team.purkynova.com.meetingschedule.Model.EventModel;
 
 
 public class EventActivity extends AppCompatActivity {
+    private EventModel eventModel;
+    TextView pleace ;
+    TextView time;
+    TextView text;
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+        pleace = (TextView) findViewById(R.id.textView);
+        time = (TextView) findViewById(R.id.textView2);
+        text = (TextView) findViewById(R.id.textView3);
+        eventModel = new EventModel(this);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            int Value = extras.getInt("id");
+            if (Value > 0) {
+                Event event = this.eventModel.get(Value);
+                if (event != null) {
+                    Log.i("test", event.getName());
+                }
 
 
+                String nam = "U69";
+                String phon = "16.00-17.00";
+                String emai = "sogjsrohzjgerihrtsighrtuihrtuhgrůuhgjsrůgjrosůjgorjtsghůrtsjhghůsrtjhůsrtdjh";
+                pleace.setText((CharSequence) phon);
+                   /* pleace.setFocusable(false);
+                    pleace.setClickable(false);*/
+
+                time.setText((CharSequence) emai);
+                   /* time.setFocusable(false);
+                    time.setClickable(false);*/
+
+                text.setText((CharSequence) nam);
+                   /* text.setFocusable(false);
+                    text.setClickable(false);*/
+
+
+            }
+        }
+
+
+    }
+    public  void materials(View view){
+        Uri urim = Uri.parse("https://github.com/Honny1/Materials.git"); //open materials on github add alter
+        Intent intent = new Intent(Intent.ACTION_VIEW, urim);
+        startActivity(intent);
+    }
+    public  void feedback(View view){
+        Uri urif = Uri.parse("http://www.sspbrno.cz/"); // open feedback found url for feedbacka
+        Intent intent = new Intent(Intent.ACTION_VIEW, urif);
+        startActivity(intent);
+    }
 }
