0d652a1801c39c7e80b3fc7f8aec48d78103ec85
Hony <hony.com@seznam.cz>
2017-11-03 21:23:00 +0100

    fix

--- app/src/main/java/white_team/purkynova/com/meetingschedule/LogoActivity.java
+++ app/src/main/java/white_team/purkynova/com/meetingschedule/LogoActivity.java
@@ -4,17 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class LogoActivity extends AppCompatActivity {
     private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
-        start();
+
     }
-    private void start(){
-        this.intent = new Intent(this, OverviewActivity.class);
-        intent.putExtra("date", "2017-11-06");
-        startActivity(intent);
-    }
+
 }
